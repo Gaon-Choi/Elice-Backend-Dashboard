77c114d2137c6e4d32344be7647255ed161b38a9
Gaon-Choi <choigaon1028@gmail.com>
2022-12-28 04:13:14 +0900

    initial settings for alembic

---
+++ alembic/versions/98b547cef7ce_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 98b547cef7ce
+Revises: 
+Create Date: 2022-12-28 04:10:26.264748
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '98b547cef7ce'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
