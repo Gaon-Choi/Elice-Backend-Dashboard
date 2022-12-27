75971f6f49ed3bfc8fe8ffdd9f97cdfdf7b28d36
Gaon-Choi <choigaon1028@gmail.com>
2022-12-28 05:00:39 +0900

    [feat] signup, login, logout + Session

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import redis
 import os
 import models
@@ -10,6 +11,7 @@ app.config["SECRET_KEY"] = 'd2707fea9778e085491e2dbbc73ff30e'
 db_string = "postgresql://developer:devpassword@127.0.0.1:25000/developer"
 
 engine = create_engine(db_string, echo=True)
+session = Session(bind=engine)
 
 @app.route('/')
 def home():
@@ -19,13 +21,29 @@ def home():
 @app.route('/user', defaults={'path': ''})
 @app.route('/user/<path:path>')
 def user(path):
+    print(params['name'])
+    params = request.get_json()
     if path == 'signup':
-        print('login')
+        name = params['name']
+        email = params['email']
+        password = params['password']
+        return signup(name, email, password)
     elif path == 'login':
-        print('sign up')
+        email = params['name']
+        password = params['password']
+        return login(email, password)
     elif path == 'logout':
-        print('log out')
+        return logout()
     return ('heoo', 200)
+
+def signup(name: str, email: str, password: str):
+    pass
+
+def login(email: str, password: str):
+    pass
+
+def logout():
+    pass
 
 if __name__ == '__main__':
     models.Base.metadata.create_all(bind=engine)
